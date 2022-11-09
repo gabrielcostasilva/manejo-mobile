25534410cf5a27d226468d225be0f88823fa8ebd
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-11-09 13:41:30 -0300

    Added cognito auth w/ Amplify

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <authenticator>
+        <ion-router-outlet />
+    </authenticator>
   </ion-app>
 </template>
 
 <script setup>
 import { IonApp, IonRouterOutlet } from '@ionic/vue'
+import { Authenticator } from '@aws-amplify/ui-vue'
 </script>
--- src/main.js
+++ src/main.js
@@ -5,20 +5,31 @@ import router from './router'
 import { IonicVue } from '@ionic/vue'
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
+import '@ionic/vue/css/core.css'
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
+import '@ionic/vue/css/padding.css'
+import '@ionic/vue/css/float-elements.css'
+import '@ionic/vue/css/text-alignment.css'
+import '@ionic/vue/css/text-transformation.css'
+import '@ionic/vue/css/flex-utils.css'
+import '@ionic/vue/css/display.css'
+
+import "@aws-amplify/ui-vue/styles.css";
+import { Amplify, Auth } from 'aws-amplify'
+
+Amplify.configure({
+  Auth: {
+    region: process.env.VUE_APP_REGION,
+    userPoolId: process.env.VUE_APP_USER_POOL_ID,
+    userPoolWebClientId: process.env.VUE_APP_USER_POOL_WEB_CLIENT_ID,
+  },
+})
 
 const app = createApp(App).use(IonicVue).use(router)
 
