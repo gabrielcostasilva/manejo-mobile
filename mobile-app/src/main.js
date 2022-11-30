552747b041f139d9e608627596b491ad59ab962a
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-11-30 17:53:09 -0300

    Removed 'aws_appsync_apiKey' from config file - This closes #2

--- mobile-app/src/main.js
+++ mobile-app/src/main.js
@@ -35,8 +35,7 @@ Amplify.configure({
       process.env.VUE_APP_AWS_APPSYNC_GRAPHQL_ENDPOINT,
     aws_appsync_region: process.env.VUE_APP_AWS_APPSYNC_REGION,
     aws_appsync_authenticationType:
-      process.env.VUE_APP_AWS_APPSYNC_AUTHENTICATION_TYPE,
-    aws_appsync_apiKey: process.env.VUE_APP_AWS_APPSYNC_API_KEY,
+      process.env.VUE_APP_AWS_APPSYNC_AUTHENTICATION_TYPE
   },
 })
 
